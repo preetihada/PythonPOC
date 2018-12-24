953d926f6cc6ea83b60b0c2f96348e73fa55ca8a
Sachinhada <45898104+Sachinhada@users.noreply.github.com>
2018-12-24 15:27:40 +0530

    Updated Code as per last Discussion
    Source code file

---
+++ Main.py
@@ -0,0 +1,108 @@
+
+##import io
+##my_file_handle=open('c:\\code\\SAPPM.txt')
+##my_file_handle.read()
+##for line in my_file_handle:
+## print(line)
+##my_file_handle.close()
+####
+##import io
+##import hashlib
+##import datetime
+##hash_object = hashlib.md5((datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+##    print(hash_object.hexdigest())
+
+import datetime
+import time
+import hashlib
+import sys
+# Open the file with read only permit Step1
+f = open('c:\\code\\TEST.txt')
+outF = open('c:\\code\\TEST_HASH.txt', "w")
+# use readline() to read the first line 
+line = f.readline()
+# use the read line to read further.
+# If the file is not empty keep reading one line
+# at a time, till the file is empty 
+while line:
+    # in python 2+
+    # print line
+    # in python 3 print is a builtin function, so
+    # open a (new) file to write
+    dtstamp = str(time.time())
+    line = line + dtstamp
+    #print(line)
+    hash_object = hashlib.md5(line.encode())
+    #outF.write(line)
+    outF.writelines(hash_object.hexdigest())
+    outF.write("\n")
+    
+    #print(line)
+    # use realine() to read next line
+    line = f.readline()
+f.close()
+
+outF.close()
+
+#datetime hashcode generation
+outFn = open('c:\\code\\TEST_HASH.txt', "a+")
+dtstamp = str(time.time())
+#print(dtstamp)
+#dtstamp = str(time.time())
+hash_object1 = hashlib.md5(dtstamp.encode())
+outFn.writelines(hash_object1.hexdigest())
+outFn.close()
+
+#LGRhashcode for filename Step2
+fname = 'TEST_HASH.txt'
+fname = fname + str(time.time())
+hash_object2 = hashlib.md5(fname.encode())
+outFn1 = open('c:\\code\\TEST_HASH_HASH.txt', "w")
+outFn1.writelines(hash_object2.hexdigest())
+outFn1.close()
+
+#HASH CODE for Filename with TR Step3
+# Open the file with read only permit
+f = open('c:\\code\\TEST_HASH_HASH.txt')
+outF = open('c:\\code\\LGRhashfile.txt', "w")
+# use readline() to read the first line
+line = f.readline()
+
+file_textSr = input(" Enter Sr.No: ")
+file_textFname = input(" Enter From : ")
+file_textFTo = input(" Enter TO : ")
+File_TR = input(" Enter T Or R : ")
+
+Str = file_textSr+' ' + file_textFname+ ' ' +file_textFTo+ ' ' +File_TR+ ' '+ line+' '+str(datetime.datetime.now())
+
+outF.write(Str)
+print("File Hashcode Generated Successfully")
+
+
+##line = f.readline()
+
+### use the read line to read further.
+### If the file is not empty keep reading one line
+### at a time, till the file is empty 
+##while line:
+##    # in python 2+
+##    # print line
+##    # in python 3 print is a builtin function, so
+##    # open a (new) file to write
+##    dtstamp = str(time.time())
+##    line = line + dtstamp
+##    #print(line)
+##    hash_object = hashlib.md5(line.encode())
+##    #outF.write(line)
+##    outF.writelines(hash_object.hexdigest())
+##    outF.write("\n")
+##    
+##    #print(line)
+##    # use realine() to read next line
+##    line = f.readline()
+##f.close()
+
+outF.close()
+
+
+
